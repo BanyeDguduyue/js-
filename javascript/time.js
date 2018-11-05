04ca359bd9cf372e2065fb202c559650d9daa8a3
半夜孤独月 <42996957+BanyeDguduyue@users.noreply.github.com>
2018-11-05 21:23:24 +0800

    Add files via upload

--- javascript/time.js
+++ javascript/time.js
@@ -1,6 +1,9 @@
 window.onload = function () {
+    var date= new Date();//获取系统的时间
+    var str =toDou(date.getHours())+":"+toDou(date.getMinutes())+":"+toDou(date.getSeconds());//将时间分钟秒变为一个6位的字符串  加两个冒号增加
     var imgs = document.getElementsByTagName("img");
 //由于str要为6位数的字符串所以将n前面补0，加上字符串的引号。
+
     function toDou(n) {
         if (n<10) {
             return "0"+n;
@@ -8,10 +11,7 @@ window.onload = function () {
             return ""+n;
         }
     }
-    setInterval(function(){  for (var i=0;i<imgs.length;i++){
-      //必须写在这里面  因为str每次都要变  所以每次都要刷新
-        var date= new Date();//获取系统的时间
-        var str =toDou(date.getHours())+":"+toDou(date.getMinutes())+":"+toDou(date.getSeconds());//将时间分钟秒变为一个6位的字符串  加两个冒号增加
+    function clock(){  for (var i=0;i<imgs.length;i++){
         imgs[i].src = 'images/'+str[i]+'.jpg';
         if(i==2 || i==5){//防止第三张图片和第6张图片发生变化
             imgs[2].src = "images/colon.jpg";
@@ -19,5 +19,8 @@ window.onload = function () {
         }else{
             imgs[i].src = 'images/'+str[i]+'.jpg';
         }
-    } }, 1000);
+      }
+    }
+    setInterval(clock, 500);
+    clock();
 };
